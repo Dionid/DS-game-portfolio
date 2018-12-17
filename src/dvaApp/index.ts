9296760163f06a0bf3d4767ea86f334d8292fdbf
Dionid <don.diodos@gmail.com>
2018-12-17 18:28:14 +0300

    Added redux immutable state checker

--- src/dvaApp/index.ts
+++ src/dvaApp/index.ts
@@ -1,5 +1,12 @@
 import dva from "dva"
+import rImS from "redux-immutable-state-invariant"
 
-const dvaApp = dva()
+const middlewares = process.env.NODE_ENV !== "production" ?
+    [rImS()] :
+    []
+
+const dvaApp = dva({
+    onAction: middlewares,
+})
 
 export default dvaApp
--- src/global.d.ts
+++ src/global.d.ts
@@ -1,2 +1,3 @@
 declare module "dva-loading"
 declare module "dva/saga"
+declare module "redux-immutable-state-invariant"
