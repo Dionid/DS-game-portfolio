6ea41590beffa7dca88c854b96d0ddfdfbb4010b
Dionid <don.diodos@gmail.com>
2018-12-18 19:20:30 +0300

    Added left and right menu

--- src/layouts/Main.tsx
+++ src/layouts/Main.tsx
@@ -24,13 +24,43 @@ class MainLayout extends React.Component<IProps, IState> {
                     <TopMenu/>
                     <div className={ cx("content") }>
                         <div className={ cx("leftMenu") }>
-
+                            <div className={ cx("leftMenuContent") }>
+                                <div className={ cx("contacts") }>
+                                    <div className={ cx("item") }>
+                                        C
+                                    </div>
+                                    <div className={ cx("item") }>
+                                        C
+                                    </div>
+                                    <div className={ cx("item") }>
+                                        C
+                                    </div>
+                                </div>
+                                <nav className={ cx("nav") }>
+                                    <div className={ cx("item") }>
+                                        Contacts
+                                    </div>
+                                    <div className={ cx("item") }>
+                                        Projects
+                                    </div>
+                                    <div className={ cx("item", "active") }>
+                                        Services
+                                    </div>
+                                </nav>
+                            </div>
                         </div>
                         <div className={ cx("gameContainer") }>
 
                         </div>
                         <div className={ cx("rightMenu") }>
-
+                            <div className={ cx("rightMenuContent") }>
+                                <a href="" className={ cx("textVersionLink") }>
+                                    TEXT Version
+                                </a>
+                                <div className={ cx("copyright") }>
+                                	copyright @2019
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
